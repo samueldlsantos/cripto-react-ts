b5720069cb1dbc13fe8fc743a6e87c37e41f473e
Samuel De los Santos Ruiz <samuel.delossantos@fiscalianl.gob.mx>
2024-09-25 16:00:58 -0600

    Se crea formulario

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -4,11 +4,20 @@ const CriptoSearchForm = () => {
     <form>
         <div className="input-group">
             <label htmlFor="currency">Moneda:</label>
-            <select name="currency">
-                <option value="">-- seleccione --</option>
+            <select className="select" name="currency">
+                <option value="">-- Seleccione --</option>
                 <option value="">MX</option>
             </select>
-        </div>    
+        </div>
+        <div className="input-group">
+            <label htmlFor="cripto">Criptomoneda:</label>
+            <select className="select" name="currency">
+                <option value="">-- Seleccione --</option>
+                <option value="">MX</option>
+            </select>
+        </div>
+
+        <input className="submit" type="submit" value="Cotizar" />     
     </form>
   )
 }
